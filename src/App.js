f3b586283fa1cf2ccf58e6cfc176df4e5e6b428b
QuangThuan <tranquanthuan132@gmail.com>
2023-05-14 01:45:29 +0700

    fix image

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        return <Route path={route.path} element={<Page />} />;
                     })}
                 </Routes>
             </div>
